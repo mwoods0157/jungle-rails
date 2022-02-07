78d481ef608596f20d6d36f054858282570176a4
matthew woods <matthew.woods0157@gmail.com>
2022-02-07 22:47:48 +0000

    Adding the login and submit functionality

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
