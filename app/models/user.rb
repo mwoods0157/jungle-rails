0b3e62cd07c19431d924f4d5c28ecb336cac005a
matthew woods <matthew.woods0157@gmail.com>
2022-02-08 19:25:50 +0000

    Added authenticate with credentials to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
     validates :email, presence: true, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }
     validates :password_confirmation, presence: true, length: { minimum: 6 }
+
+    def self.authenticate_with_credentials(email, password)
+        email = email.downcase.strip
+        @user = User.find_by_email(email)
+        if @user && @user.authenticate(password)
+            @user
+        else
+            nil
+        end
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -62,5 +62,25 @@ RSpec.describe User, type: :model do
       )
       expect(user.valid?).to be false
     end
-    
+    it "user password should be longer than 6 characters" do
+      user = User.new(
+        first_name: 'matt',
+        last_name: 'woods',
+        email: 'test@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(user.password >= 6).to be true
+    end
+    it "should return error if password less than 6 characters" do
+      user = User.new(
+        first_name: 'matt',
+        last_name: 'woods',
+        email: 'test@email.com',
+        password: 'pass',
+        password_confirmation: 'pass'
+      )
+      expect(user.password_confirmation >= 6).to be false
+    end
+    it 
 end
